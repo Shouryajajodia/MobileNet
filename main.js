fac6a70978f066d3264ba4aea43ab2477fe31128
Shouryajajodia <87896065+Shouryajajodia@users.noreply.github.com>
2021-08-28 15:03:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,34 @@ Webcam.set({
     height: 300,
     image_format: 'png',
     png_quality: 90,
-    constraints:{
-    facingMode: "environment"
+    constraints: {
+        facingMode: "environment"
     }
 });
 camera = document.getElementById("camera");
 Webcam.attach('#camera');
+
+function takesnapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+console.log("ml5 version",ml5.version);
+classifier=ml5.imageClassifier("MobileNet",model_loaded);
+
+function model_loaded(){
+    console.log("model is loaded");
+}
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotresult);
+}
+function gotresult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("object_name").innerHTML=results[0].label;
+    }
+}
